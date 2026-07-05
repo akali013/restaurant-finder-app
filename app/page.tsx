a35c8b5925ee4120a2b55c3da0e8d4fe3ad9b5d7
akali013 <131390728+akali013@users.noreply.github.com>
2026-07-05 11:53:50 -0400

    Added Suspense to app/page.tsx for vercel integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import ImageScroller from "./ui/login/ImageScroller";
 import LoginForm from "./ui/login/LoginForm";
 
 export default function LoginPage() {
   return (
     <div className="grid grid-cols-2">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
       <ImageScroller />
     </div>
   );
